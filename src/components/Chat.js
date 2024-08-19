120e09fdc232345296b2a8761463bafa5ee25ac9
BenGix <84526926+BenGix@users.noreply.github.com>
2024-08-19 08:46:50 +0330

    fix chat page

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,11 +5,11 @@ export const Chat = () => {
   return (
     <section className="h-full flex flex-col bg-white p-4">
       <div className="flex-grow">
-        <div className="flex items-start gap-2.5">
-          <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 bg-light rounded-e-xl rounded-es-xl">
+        <div className="flex justify-end gap-2.5">
+          <div className="flex flex-col w-full  justify-end max-w-72 leading-1.5 p-4 bg-light rounded-s-xl rounded-es-xl">
             <p className="text-sm font-normal py-2.5 text-gray-900 ">
-              awesome. I think our users will really appreciate the
-              improvements.
+              از طریق فیلد پایین صفحه، سوال خود را در مورد مجلات، نمودارها و
+              پایگاه دانش نمافر بپرسید تا روبات هوش مصنوعی ما به شما پاسخ دهد.
             </p>
           </div>
         </div>
--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -3,34 +3,35 @@ import React, { useRef } from "react";
 
 export const ChatInput = () => {
   const textareaRef = useRef(null);
+
   const autoResizeTextarea = () => {
     const textarea = textareaRef.current;
-    textarea.style.height = "auto";
-    textarea.style.height = `${textarea.scrollHeight}px`;
+    textarea.style.height = "auto"; // Reset the height to auto
+    textarea.style.height = `${textarea.scrollHeight}px`; // Set it to the scroll height, up to the max height
   };
 
   return (
-    <form>
+    <form className="min-h-10 max-h-28">
       <label htmlFor="chat" className="sr-only">
         Your message
       </label>
-      <div className="flex items-end p-1 rounded-4xl bg-lightGray">
+      <div className="flex items-end h-full p-1 rounded-3xl bg-lightGray">
         <textarea
           id="chat"
           ref={textareaRef}
           rows="1"
-          className="w-full min-h-9 bg-transparent resize-none px-4 focus-visible:ring-0 focus-visible:ring-transparent outline-none focus-visible:shadow-none focus-visible:border-0 focus-visible:outline-0"
+          className="w-full max-h-28 bg-transparent text-xs resize-none px-4 focus-visible:ring-0 focus-visible:ring-transparent outline-none focus-visible:shadow-none focus-visible:border-0 focus-visible:outline-0 overflow-auto"
           placeholder="چت با روبات نمافر"
           onInput={autoResizeTextarea}
+          style={{ maxHeight: "110px" }} // Set max height to 110px
         ></textarea>
-        <div className="h-full rounded-full bg-gradient-button">
-          <button
-            type="submit"
-            className="inline-flex h-full items-end justify-center p-2 rotate-180 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100"
-          >
-            <Send size="32" color="#FFf" variant="Outline" />
-          </button>
-        </div>
+
+        <button
+          type="submit"
+          className="flex flex-col h-full max-h-8 items-center justify-center aspect-square rounded-full bg-gradient-button rotate-180"
+        >
+          <Send size="16" color="#FFf" variant="Outline" />
+        </button>
       </div>
     </form>
   );
